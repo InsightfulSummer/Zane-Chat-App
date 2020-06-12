0d81292cf643bccbf49abe39f8f518e3593b0959
zane <ethan.zane.chiang@gmail.com>
2020-06-12 15:18:16 -0400

    index.spec.js update

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,17 +1,15 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const socket = io.connect();
 
-
-console.log('check 1', socket.connected);
-socket.on('connect', function() {
-  console.log('check 2', socket.connected);
+console.log('check 1', server.connected);
+server.on('connect', function() {
+  console.log('check 2', server.connected);
 });
 
 describe('Index tests', () => {
 
     test('test true is true', () => {
-        expect(true).toBe(true)
+        expect(server.connected).toBe(true)
     });
 });
